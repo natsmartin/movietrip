da670756aea8c523ce302f3c0a95960a7fe4dc72
natsmartin <natsmartin95@gmail.com>
2024-09-06 05:10:53 +0800

    updates

--- app/components/MovieBox/MovieBox.tsx
+++ app/components/MovieBox/MovieBox.tsx
@@ -18,10 +18,10 @@ export default function MovieBox() {
         )
     }
 
-    if (response === null || response.Error) {
+    if (!response || response.Error) {
         return (
             <div className='flex md:min-h-10 justify-center md:items-center'>
-                <h1>{response?.Error}</h1>
+                <h1>{response.Error}</h1>
             </div>
         )
     }
