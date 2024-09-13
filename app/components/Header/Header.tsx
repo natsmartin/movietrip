ad37bf8318b126dd61aeb7e75303cdaede0f7681
natsmartin <natsmartin95@gmail.com>
2024-09-13 23:29:39 +0800

    add clamp

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <header className='flex justify-center items-center w-full font-bold text-4xl min-h-12 text-center
-            md:text-7xl md:min-h-24'>
+        <header className='header-clamp flex justify-center items-center w-full font-bold 
+        min-h-12 text-center md:min-h-24'>
             MovieTrip
         </header>
     )
--- app/sections/Details/Details.tsx
+++ app/sections/Details/Details.tsx
@@ -40,12 +40,12 @@ export default function Details({ response }: { response: Movie }) {
 
     return (
         <>
-            <h1 className='font-bold text-xl text-center mb-4 md:text-left md:text-3xl'>
+            <h1 className='font-bold text-center mb-4 md:text-left heading1'>
                 {response.Title}({response.Year})
             </h1>
             {response.Ratings && response.Ratings[0] ?
                 (
-                    <h2 className='details-h2'>IMDb Rating⭐&nbsp;
+                    <h2 className='heading2 pl-[2rem] md:pl-0'>IMDb Rating⭐&nbsp;
                         <span className={`font-bold ${colorStatus(response.Ratings[0].Value)}`}>
                             {splitRating(response.Ratings[0].Value)[0]}
                         </span>
@@ -53,20 +53,20 @@ export default function Details({ response }: { response: Movie }) {
                             /{splitRating(response.Ratings[0].Value)[1]}
                         </span>
                     </h2>
-                ) : <h2 className='details-h2'>N/A⭐</h2>
+                ) : <h2 className='heading2 pl-[2rem] md:pl-0'>N/A⭐</h2>
             }
 
 
             {
                 response.Ratings && response.Ratings[1] ?
-                    <h2 className='details-h2'>{response.Ratings[1].Source}🍅&nbsp;
+                    <h2 className='heading2 pl-[2rem] md:pl-0'>{response.Ratings[1].Source}🍅&nbsp;
                         <span className={`font-bold ${colorStatus(response.Ratings[1].Value)}`} >
                             {splitPercent(response.Ratings[1].Value)[0]}%
                         </span>
-                    </h2> : <h2 className='details-h2'>N/A🍅</h2>
+                    </h2> : <h2 className='heading2 pl-[2rem] md:pl-0'>N/A🍅</h2>
             }
 
-            <h2 className='details-h2'>Rated&nbsp;
+            <h2 className='heading2 pl-[2rem] md:pl-0'>Rated&nbsp;
                 <span className='font-bold'>
                     {response.Rated}
                 </span>
@@ -74,11 +74,11 @@ export default function Details({ response }: { response: Movie }) {
             <p className='text-xs px-8 md:p-0'>Runtime: <span className='font-bold'>{response.Runtime}</span></p>
             <p className='text-xs pl-8 md:p-0'>Released Date: <span className='font-bold'>{response.Released}</span></p>
             <br></br>
-            <p className='text-xs md:text-base'><span className='font-bold'>Director: </span>{response.Director}</p>
-            <p className='text-xs md:text-base'><span className='font-bold'>Writer: </span>{response.Writer}</p>
-            <p className='text-xs md:text-base'><span className='font-bold'>Actors: </span>{response.Actors}</p>
-            <p className='text-xs md:text-base'><span className='font-bold'>Genre: </span>{response.Genre}</p>
-            <p className='text-xs md:w-1/2 md:text-base'><span className='font-bold'>Plot: </span>{response.Plot}</p>
+            <p className='normal-text'><span className='font-bold'>Director: </span>{response.Director}</p>
+            <p className='normal-text'><span className='font-bold'>Writer: </span>{response.Writer}</p>
+            <p className='normal-text'><span className='font-bold'>Actors: </span>{response.Actors}</p>
+            <p className='normal-text'><span className='font-bold'>Genre: </span>{response.Genre}</p>
+            <p className='normal-text md:w-1/2'><span className='font-bold'>Plot: </span>{response.Plot}</p>
         </>
     )
 }
