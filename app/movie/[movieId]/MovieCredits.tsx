114aac1c6477d547be20483603fc23539bc24634
natsmartin <107980240+natsmartin@users.noreply.github.com>
2025-04-22 13:45:12 +0800

    fix layout for cast

--- app/movie/[movieId]/MovieCredits.tsx
+++ app/movie/[movieId]/MovieCredits.tsx
@@ -9,7 +9,11 @@ const MovieCredits = ({ movieCast }: { movieCast: any }) => {
           <h2 className="w-full font-bold text-center text-base py-2 md:text-xl">
             Casts
           </h2>
-          <div className="flex overflow-x-scroll *:mx-1 *:my-3">
+          <div
+            className={`flex overflow-x-scroll *:mx-1 *:my-3 ${
+              movieCast.length < 11 ? "md:justify-center md:overflow-hidden" : ""
+            }`}
+          >
             {movieCast.map((cast: any, index: number) => (
               <MovieCast key={index} cast={cast} />
             ))}
